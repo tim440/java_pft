11eebbbda30f0dd4cf7fd5654e44c3cc5234c237
tim440 <tim440@yandex.ru>
2017-09-12 03:58:50 +0300

    изменена функция distance

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,6 +4,7 @@ import java.awt.*;
 
 public class MyFirstProgram {
 
+
   public static void main(String[] args) {
     hello("world");
     double l = 12;
@@ -12,14 +13,11 @@ public class MyFirstProgram {
     double a = 5;
     double b = 6;
     System.out.println("Площадь прямоугольника со сторонами " + a + " " + b + " = " + rec(a, b));
-    Point p1 = new Point(2, 3);
-    Point p2 = new Point(5, 6);
+    Point pa = new Point(2, 3);
+    Point pb = new Point(2, 3);
+    System.out.println("Расстоняние между точками " + pa + " " + pb + " = " + distance(pa, pb));
 
-    System.out.println("Расстоняние между точками " + p1 + " " + p2 + " = " + distance(p1, p2));
-    //Point p3 = new Point(3,4.67);
-   // System.out.println("Координаты точки: "+p3.toString());
   }
-
 
 
   public static void hello(String smb) {
@@ -35,10 +33,11 @@ public class MyFirstProgram {
   }
 
   public static double distance(Point p1, Point p2) {
-    double a = p1.x - p2.x;
-    double b = p1.y - p2.y;
-    return Math.sqrt(a * a + b * b);
+    double x = p1.x - p2.x;
+    double y = p1.y - p2.y;
+    return Math.sqrt(x * x + y * y);
   }
+
 
 }
 
