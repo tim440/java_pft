289413e8e3bdc4e347706a658f183f20c55069a1
tim440 <tim440@yandex.ru>
2017-09-14 20:52:16 +0300

    Добавлены отрицательные тесты

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -5,9 +5,21 @@ import org.testng.annotations.Test;
 
 public class PointTests {
   @Test
-  public void testPoint(){
+  public void testPointTrue(){
     Point pa = new Point(2,3);
     Point pb = new Point (6,6);
     Assert.assertEquals (Point.distance(pa, pb), 5.0);
+  }
+  @Test
+  public void testPointFalse1(){
+    Point pa = new Point(2,3);
+    Point pb = new Point (6,6);
+    Assert.assertEquals (Point.distance(pa, pb), 6.0);
+  }
+  @Test
+  public void testPointFalse2(){
+    Point pa = new Point(2,3);
+    Point pb = new Point (6,6);
+    Assert.assertEquals (Point.distance(pa, pb), 32);
   }
 }
