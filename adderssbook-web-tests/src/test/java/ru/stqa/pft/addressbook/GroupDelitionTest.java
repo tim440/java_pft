8e9b1f3e105ebad11399809514f5e4619b82caba
tim440 <tim440@yandex.ru>
2017-09-15 16:59:55 +0300

    Создан тест удаления группы, создан базовый класс

---
+++ adderssbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDelitionTest.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.addressbook;
+
+
+import org.testng.annotations.Test;
+
+
+public class GroupDelitionTest extends TestBase {
+
+
+  @Test
+  public void GroupDelitionTest() {
+    gotoGroupPage();
+
+    selectGroup();
+    deleteSelectedGroups();
+    returnToGroupPage();
+  }
+
+}
