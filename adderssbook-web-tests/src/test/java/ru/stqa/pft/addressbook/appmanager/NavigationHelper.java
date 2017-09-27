a51937d4ac4a00338128d29eac6426984a5be1df
tim440 <tim440@yandex.ru>
2017-09-27 22:11:00 +0300

    Выполены заданя 7,8

--- adderssbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ adderssbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -2,9 +2,8 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class NavigationHelper extends HelperBase{
+public class NavigationHelper extends HelperBase {
 
 
   public NavigationHelper(WebDriver wd) {
@@ -12,10 +11,18 @@ public class NavigationHelper extends HelperBase{
   }
 
   public void gotoGroupPage() {
-    click(By.linkText("groups"));
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new"))){
+      return;
+    }
+      click(By.linkText("groups"));
   }
 
   public void gotoHomePage() {
+    if (isElementPresent(By.id("maintable"))){
+      return;
+    }
     click(By.linkText("home"));
   }
 }
