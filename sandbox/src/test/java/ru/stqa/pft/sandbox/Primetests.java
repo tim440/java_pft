a586189fad146ecdf7c9d67ddd1c5983c45fadfd
tim440 <tim440@yandex.ru>
2017-10-06 01:50:11 +0300

    less4 m2

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/Primetests.java
@@ -0,0 +1,23 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Primetests {
+  @Test
+  public void testPrimes() {
+    Assert.assertTrue(Primes.isPrimeFast(Integer.MAX_VALUE));
+  }
+
+  @Test
+  public void testNonPrime() {
+    Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE - 2));
+  }
+
+  @Test(enabled = false)
+  public void testPrimesLong() {
+    long n = Integer.MAX_VALUE;
+
+    Assert.assertTrue(Primes.isPrime(n));
+  }
+}
