7332e65f1ca0aef8f6876119e31c007d91d735e0
tim440 <tim440@yandex.ru>
2017-10-08 13:32:54 +0300

    less4 m4

--- adderssbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ adderssbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -27,9 +27,10 @@ public class GroupHelper extends HelperBase {
     click(By.name("delete"));
   }
 
-  public void selectGroup() {
+  public void selectGroup(int index) {
     if (!wd.findElement(By.name("selected[]")).isSelected()) {
-      click(By.name("selected[]"));
+      wd.findElements(By.name("selected[]")).get(index).click();
+      //click(By.name("selected[]"));
     }
   }
 
