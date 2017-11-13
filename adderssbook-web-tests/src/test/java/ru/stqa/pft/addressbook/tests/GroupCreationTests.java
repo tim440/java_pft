21cf94f6bae200347163676bcdd6e95b4b08b2aa
tim440 <tim440@yandex.ru>
2017-11-13 03:19:19 +0300

    Изменения по уроку 5 м6

--- adderssbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ adderssbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,11 +1,11 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 
-import java.util.HashSet;
-import java.util.Set;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
   @Test
@@ -14,16 +14,14 @@ public class GroupCreationTests extends TestBase {
     for (int i = 1; i <= n; i++) {
 
       app.goTo().groupPage();
-      Set<GroupData> before = app.group().all();
+      Groups before = app.group().all();
       GroupData group = new GroupData().withName("test2");
       app.group().create(group);
-      Set<GroupData> after = app.group().all();
-      Assert.assertEquals(after.size(), before.size() + 1);
+      Groups after = app.group().all();
+      assertThat(after.size(),equalTo( before.size() + 1));
+      assertThat(after, equalTo(
+              before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
 
-group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());
-      before.add(group);
-
-      Assert.assertEquals(new HashSet<Object>(after), new HashSet<Object>(before));
     }
   }
 }
--- adderssbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTest.java
+++ adderssbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTest.java
@@ -1,11 +1,18 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 
 import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertEquals;
 
 public class GroupDeletionTest extends TestBase {
   @BeforeMethod
@@ -22,16 +29,16 @@ public class GroupDeletionTest extends TestBase {
     //  int n = 3; // Количество удаляемых групп
     // for (int i = 1; i <= n; i++) {
 
-    Set<GroupData> before = app.group().all();
+    Groups before = app.group().all();
     GroupData deletedGroup = before.iterator().next();
     app.group().delete(deletedGroup);
-    Set<GroupData> after = app.group().all();
-    Assert.assertEquals(after.size(), before.size() - 1);
+    Groups after = app.group().all();
+    assertEquals(after.size(), before.size() - 1);
 
-    before.remove(deletedGroup);
+
     //before.remove(before.size()-1);
+assertThat(after, equalTo(before.without(deletedGroup)));
 
-    Assert.assertEquals(after, before);
 
     app.goTo().gotoHomePage();
     // }
