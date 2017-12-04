812df9df06c12a25b0c4e26d6b784ae82544fb0c
tim440 <tim440@yandex.ru>
2017-12-04 04:37:51 +0300

    Изменения по уроку 6 м7 - не работает

--- adderssbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ adderssbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -18,27 +20,42 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validGroups() throws IOException {
+  public Iterator<Object[]> validGroupsFromXml() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
-    BufferedReader reader= new BufferedReader( new FileReader(new File("src/test/resources/groups.xml")));
-    String xml ="";
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
+    String xml = "";
 
     String line = reader.readLine();
-    while (line != null && !"".equals(line.trim())){
-      xml +=line;
-    //  String[] split = line.split(";"); // Для файлов CSV
+    while (line != null && !"".equals(line.trim())) {
+      xml += line;
+      //  String[] split = line.split(";"); // Для файлов CSV
       //list.add (new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
       line = reader.readLine();
     }
     XStream xStream = new XStream();
     xStream.processAnnotations(GroupData.class);
     List<GroupData> groups = (List<GroupData>) xStream.fromXML(xml);
-    return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
-
+    return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+  }
+    @DataProvider
+    public Iterator<Object[]> validGroupsFromJson() throws IOException {
+      List<Object[]> list = new ArrayList<Object[]>();
+      BufferedReader reader= new BufferedReader( new FileReader(new File("src/test/resources/groups.json")));
+      String json ="";
+      String line = reader.readLine();
+      while (line != null && !"".equals(line.trim())){
+        json +=line;
+        //  String[] split = line.split(";"); // Для файлов CSV
+        //list.add (new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+        line = reader.readLine();
+      }
+      Gson gson = new Gson();
+      List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>(){}.getType());
+      return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
 
   }
 
-  @Test (dataProvider = "validGroups")
+  @Test (dataProvider = "validGroupsFromJson")
   public void testGroupCreation(GroupData group) {
 
 
